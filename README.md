653681e7561f06eaa2c687d25bc4bce9625716fc
David Hain <d.hain@gmx.at>
2023-06-19 19:16:47 +0200

    feat: parsing hyprctl workspaces

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# hyprland_workspace_display
-Command line utility to display hyprland workspaces.
+# Hyprland Workspace Display
+
+```shell
+hyprspacedp --empty-workspace-symbol o \
+            --empty-workspace-color #696969 \
+            --full-workspace-symbol O \
+            --full-workspace-color #424242
+            --active-workspace-symbol x \
+            --active-workspace-color #420690
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use serde::{Deserialize, Serialize};
+use std::process::Command;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Workspace {
+    id: u8,
+    name: String,
+    monitor: String,
+    windows: u32,
+    #[serde(rename = "hasfullscreen")]
+    has_fullscreen: bool,
+    #[serde(rename = "lastwindow")]
+    last_window: String,
+    #[serde(rename = "lastwindowtitle")]
+    last_window_title: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let workspaces = Command::new("hyprctl")
+        .arg("workspaces")
+        .arg("-j")
+        .output()
+        .expect("hyprctl failed to run");
+
+    let workspaces: Vec<Workspace> =
+        serde_json::from_slice(&workspaces.stdout).expect("hyprctl had no output");
 }
