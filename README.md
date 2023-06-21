60217be6ae3f588f6136352cab3e6b1356c21b28
David Hain <d.hain@gmx.at>
2023-06-21 09:57:49 +0200

    feat: added unicode support, removed colors

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Except someone wants to make a pull request to add that functionality.
 
 Config file in `${XDG_CONFIG_HOME}/hypr/hyprspacedp/config.toml`.
 
-For a unicode icon use `\u{UNICODE}` like for example the Rust logo: `\u{e7a8}`.
+Unicode icons are also supported.
 
 ```shell
 hyprspacedp --workspace-amount 10 \
             --empty-workspace-symbol o \
-            --full-workspace-symbol "O" \
-            --active-workspace-symbol "x" \
+            --full-workspace-symbol 'O' \
+            --active-workspace-symbol "" \
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use anyhow::Context;
 use clap::Parser;
-use hex_color::HexColor;
 use serde::{Deserialize, Serialize};
-use std::{process::Command};
+use std::process::Command;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct Workspace {
@@ -29,27 +29,15 @@ struct Args {
 
     /// Symbol for a workspace that does not contain any windows.
     #[clap(long)]
-    empty_workspace_symbol: String,
-
-    /// Hex color for a workspace that does not contain any windows.
-    #[clap(long, default_value_t = HexColor::WHITE)]
-    empty_workspace_color: HexColor,
+    empty_workspace_symbol: char,
 
     /// Symbol for a workspace that contains one or more windows.
     #[clap(long)]
-    full_workspace_symbol: String,
-
-    /// Hex color for a workspace that contains one or more windows.
-    #[clap(long, default_value_t = HexColor::WHITE)]
-    full_workspace_color: HexColor,
+    full_workspace_symbol: char,
 
     /// Symbol for a workspace that is currently being displayed.
     #[clap(long)]
-    active_workspace_symbol: String,
-
-    /// Hex color for a workspace that is currently being displayed.
-    #[clap(long, default_value_t = HexColor::WHITE)]
-    active_workspace_color: HexColor,
+    active_workspace_symbol: char,
 }
 
 fn main() {
@@ -58,31 +46,32 @@ fn main() {
     let workspaces = get_workspaces().expect("got no workspaces");
     let active_workspace = get_active_workspace().expect("got no active workspace");
     let mut print_buffer = String::new();
+
     for idx in 1..=args.workspace_amount {
-        dbg!(idx);
         if let Some(workspace) = get_workspace_by_id(idx, &workspaces) {
             if workspace.id == active_workspace.id {
-                print_buffer.push_str(&args.active_workspace_symbol);
+                print_buffer.push(args.active_workspace_symbol);
             } else if workspace.id == idx {
-                print_buffer.push_str(&args.full_workspace_symbol);
+                print_buffer.push(args.full_workspace_symbol);
             }
         } else {
+            print_buffer.push(args.empty_workspace_symbol);
+        }
 
-                print_buffer.push_str(&args.empty_workspace_symbol);
-            }
+        print_buffer.push(' ');
     }
 
     println!("{}", print_buffer);
 }
 
-fn get_workspaces() -> Option<Vec<Workspace>> {
+fn get_workspaces() -> anyhow::Result<Vec<Workspace>> {
     let workspaces = Command::new("hyprctl")
         .arg("workspaces")
         .arg("-j")
         .output()
-        .expect("hyprctl failed to run");
+        .context("hyprctl failed to run");
 
-    serde_json::from_slice(&workspaces.stdout).ok()
+    Ok(serde_json::from_slice(&workspaces?.stdout)?)
 }
 
 fn get_workspace_by_id(id: u8, workspaces: &Vec<Workspace>) -> Option<Workspace> {
