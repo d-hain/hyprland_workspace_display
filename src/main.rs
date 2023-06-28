4181821f4cfce6ce820082b165844a3d8c00d5a2
David Hain <d.hain@gmx.at>
2023-06-28 10:17:13 +0200

    feat: basic eww widgets with {NR} replacing completed

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,126 @@ mod symbols;
 
 /// Outputs symbols or eww widgets for each workspace, depending on them
 /// containing a window or being the currently active one.
-#[derive(Parser, Debug)]
+#[derive(Parser, Clone, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Amount of workspace symbols to be displayed.
-    #[clap(short = 'n', long)]
+    #[arg(short = 'n', long, required = true, value_name = "AMOUNT")]
     workspace_amount: u8,
 
     /// Symbol for a workspace that does not contain any windows.
-    #[clap(long)]
+    #[arg(long, required = true, value_name = "SYMBOL")]
     empty_symbol: char,
 
     /// Symbol for a workspace that contains one or more windows.
-    #[clap(long)]
+    #[arg(long, required = true, value_name = "SYMBOL")]
     full_symbol: char,
 
     /// Symbol for a workspace that is currently active.
-    #[clap(long)]
+    #[arg(long, required = true, value_name = "SYMBOL")]
     active_symbol: char,
 
+    /// Runs continuously and prints on change in workspaces.
+    #[arg(short = 'p', long)]
+    polling: bool,
+
     /// Return eww widgets (buttons) instead of only symbols for each workspace.
-    #[clap(long)]
+    #[arg(long)]
     eww_widgets: bool,
 
-    /// Runs continuously and prints on change in workspaces.
-    #[clap(short = 'p', long)]
-    polling: bool,
+    /// CSS class to use for the box.
+    #[arg(long, requires = "eww_widgets", value_name = "CLASS")]
+    eww_class_box: Option<String>,
+
+    /// Valign to use for the box.
+    #[arg(
+        long,
+        requires = "eww_widgets",
+        value_name = "VALIGN",
+        value_parser = clap::builder::PossibleValuesParser::new([
+            "fill",
+            "baseline",
+            "center",
+            "start",
+            "end",
+        ])
+    )]
+    eww_valign: Option<String>,
+
+    /// Halign to use for the box.
+    #[arg(
+        long,
+        requires = "eww_widgets",
+        value_name = "HALIGN",
+        value_parser = clap::builder::PossibleValuesParser::new([
+            "fill",
+            "baseline",
+            "center",
+            "start",
+            "end",
+        ])
+    )]
+    eww_halign: Option<String>,
+
+    /// Spacing to use for the box.
+    #[arg(long, requires = "eww_widgets", value_name = "SPACING")]
+    eww_spacing: Option<u32>,
+
+    /// Orientation to use for the box.
+    #[arg(
+        long,
+        requires = "eww_widgets",
+        value_name = "ORIENTATION",
+        value_parser = clap::builder::PossibleValuesParser::new([
+            "horizontal",
+            "h",
+            "vertical",
+            "v",
+        ])
+    )]
+    eww_orientation: Option<String>,
+
+    /// CSS class to use for the button.
+    #[arg(
+        long,
+        requires = "eww_widgets",
+        value_name = "CLASS",
+        long_help = "
+        It is possible to specify the workspace id by using {NR}.\r
+        {NR} will be replaced by the id of the workspace."
+    )]
+    eww_class_button: Option<String>,
+
+    /// Onclick command to execute for each button.
+    #[arg(
+        long,
+        requires = "eww_widgets",
+        value_name = "ONCLICK",
+        long_help = "
+        It is possible to specify the workspace id by using {NR}.\r
+        {NR} will be replaced by the id of the workspace."
+    )]
+    eww_onclick: Option<String>,
+
+    #[arg(
+        long,
+        requires = "eww_widgets",
+        value_name = "ONMIDDLECLICK",
+        long_help = "
+        It is possible to specify the workspace id by using {NR}.\r
+        {NR} will be replaced by the id of the workspace."
+    )]
+    eww_onmiddleclick: Option<String>,
+
+    #[arg(
+        long,
+        requires = "eww_widgets",
+        value_name = "ONRIGHTCLICK",
+        long_help = "
+        It is possible to specify the workspace id by using {NR}.\r
+        {NR} will be replaced by the id of the workspace."
+    )]
+    eww_onrightclick: Option<String>,
 }
 
 fn main() {
@@ -50,7 +144,7 @@ fn main() {
     );
 
     if args.polling {
-        print_symbols(&symbols, args.eww_widgets);
+        print_symbols(&symbols, &args);
 
         loop {
             let workspaces_polling = get_workspaces().expect("got no workspaces");
@@ -69,11 +163,11 @@ fn main() {
                     &active_workspace,
                 );
 
-                print_symbols(&symbols, args.eww_widgets);
+                print_symbols(&symbols, &args);
             }
         }
     } else {
-        print_symbols(&symbols, args.eww_widgets);
+        print_symbols(&symbols, &args);
     }
 }
 
@@ -83,26 +177,99 @@ fn main() {
 ///
 /// - `workspace_symbols` - the current symbols for all workspaces.
 /// - `eww_widgets` - wheter to print eww widgets or normal symbols.
-fn print_symbols(workspace_symbols: &Vec<char>, eww_widgets: bool) {
-    if eww_widgets {
-        for (mut idx, symbol) in workspace_symbols.iter().enumerate() {
-            idx += 1;
-
-            println!(
-                "(button \
-                    :onclick \"hyprctl dispatch workspace {}\" \
-                    :class \"workspace-{}\" \
-                    :halign \"center\" \
-                    \"{}\")",
-                idx, idx, symbol
-            );
-        }
+fn print_symbols(workspace_symbols: &Vec<char>, args: &Args) {
+    if args.eww_widgets {
+        println!(
+            "{}",
+            get_eww_widget(
+                workspace_symbols,
+                &args.eww_class_box,
+                &args.eww_valign,
+                &args.eww_halign,
+                &args.eww_spacing,
+                &args.eww_orientation,
+                &args.eww_class_button,
+                &args.eww_onclick,
+                &args.eww_onmiddleclick,
+                &args.eww_onrightclick,
+            )
+        );
     } else {
         workspace_symbols
             .iter()
             .for_each(|symbol| print!("{} ", symbol));
     }
+
     println!();
+}
+
+fn get_eww_widget(
+    workspace_symbols: &Vec<char>,
+    eww_class_box: &Option<String>,
+    eww_valign: &Option<String>,
+    eww_halign: &Option<String>,
+    eww_spacing: &Option<u32>,
+    eww_orientation: &Option<String>,
+    eww_class_button: &Option<String>,
+    eww_onclick: &Option<String>,
+    eww_onmiddleclick: &Option<String>,
+    eww_onrightclick: &Option<String>,
+) -> String {
+    let mut eww_widget = String::new();
+
+    let push_string_property = |mut eww_widget: String,
+                                property: &str,
+                                value: &Option<String>,
+                                nr: Option<usize>|
+     -> String {
+        if let Some(mut value) = value.clone() {
+            if let Some(nr) = nr {
+                value = value.replace("{NR}", &nr.to_string().as_str());
+            }
+            eww_widget.push_str(&format!(":{} '{}' ", property, value));
+        }
+
+        return eww_widget;
+    };
+
+    eww_widget.push_str("(box ");
+
+    // Box properties
+    eww_widget = push_string_property(eww_widget.clone(), "class", eww_class_box, None);
+    eww_widget = push_string_property(eww_widget.clone(), "valign", eww_valign, None);
+    eww_widget = push_string_property(eww_widget.clone(), "halign", eww_halign, None);
+    if let Some(spacing) = eww_spacing {
+        eww_widget.push_str(&format!(":spacing {} ", spacing));
+    }
+    eww_widget = push_string_property(eww_widget.clone(), "orientation", eww_orientation, None);
+
+    // Buttons
+    for (mut idx, symbol) in workspace_symbols.iter().enumerate() {
+        idx += 1;
+
+        eww_widget.push_str("(button ");
+
+        eww_widget = push_string_property(eww_widget.clone(), "class", eww_class_button, Some(idx));
+        eww_widget = push_string_property(eww_widget.clone(), "onclick", eww_onclick, Some(idx));
+        eww_widget = push_string_property(
+            eww_widget.clone(),
+            "onmiddleclick",
+            eww_onmiddleclick,
+            Some(idx),
+        );
+        eww_widget = push_string_property(
+            eww_widget.clone(),
+            "onrightclick",
+            eww_onrightclick,
+            Some(idx),
+        );
+
+        eww_widget.push_str(&format!("'{}')", symbol));
+    }
+
+    eww_widget.push_str(")");
+
+    eww_widget
 }
 
 /// # Returns
