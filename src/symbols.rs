4bbde132d635a5fee37d3a99129b4236b76f8c12
Nineven <nineven@localhost.localdomain>
2023-11-22 20:35:19 +0100

    fix: workspaces buttons not switching to their according workspaces

--- src/symbols.rs
+++ src/symbols.rs
@@ -7,18 +7,20 @@ pub fn get_workspace_symbols(
     full_workspace_symbol: char,
     empty_workspace_symbol: Option<char>,
     active_workspace: &Workspace,
-) -> Vec<char> {
+) -> Vec<(char, u8)> {
     let mut symbols = Vec::new();
 
     for idx in 1..=workspace_amount {
         if let Some(workspace) = get_workspace_by_id(idx, &workspaces) {
             if workspace.id == active_workspace.id {
-                symbols.push(active_workspace_symbol);
+                symbols.push((active_workspace_symbol, idx));
             } else {
-                symbols.push(full_workspace_symbol);
+                symbols.push((full_workspace_symbol, idx));
             }
-        } else if let Some(empty_symbol) = empty_workspace_symbol {
-            symbols.push(empty_symbol);
+        } else {
+            if let Some(symbol) = empty_workspace_symbol {
+                symbols.push((symbol, idx));
+            }
         }
     }
 
